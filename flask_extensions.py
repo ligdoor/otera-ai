05b33e918fad36fd87541c9156184d39570ff42a
MC_Sedil <89173139+ligdoor@users.noreply.github.com>
2026-02-11 15:37:32 +0900

    default_limitsの緩和

--- flask_extensions.py
+++ flask_extensions.py
@@ -5,6 +5,6 @@ from flask_limiter.util import get_remote_address
 limiter = Limiter(
     key_func=get_remote_address,
     storage_uri=os.environ.get("REDIS_URL", "memory://"),
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["2000 per day", "500 per hour"],
     headers_enabled=True
 )
